ed5725dc7e6dd1a62e3801b8d649158a2e67cd5b
Marcos Venancio <viniciusdacosta@hotmail.com.br>
2020-08-13 15:15:06 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,18 +1,34 @@
 package boardgame;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
-		position = null; //a posição de uma peça recem criada seria nula
+		position = null; // a posição de uma peça recem criada seria nula
 	}
 
-	protected Board getBoard() { //o tabuleiro associado a uma peça so deve ser acessado dentro do pacote board e piece
+	protected Board getBoard() { // o tabuleiro associado a uma peça so deve ser acessado dentro do pacote board e piece
 		return board;
 	}
-	
-	
+
+	public abstract boolean[][] possibleMoves(); // método abstrato que chama uma matriz de booleanos
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()]; // método concreto se utilizando de um método abstrato
+	}
+
+	public boolean isThereAnyPossibleMove() { //implementação concreta que depende de um método abstrato
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true; //se a matriz na linha i e coluna j for verdadeira um movimento é possível
+				}
+			}
+		}
+		return false;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,9 +40,12 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
-	private void validateSourcePosition(Position position) {
+	private void validateSourcePosition(Position position) { //validação da mudança de posição de peça na origem
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
